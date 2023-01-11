b4fcc7e18da3c5f45942c20350b6e427c0c28eab
luki120 <luki120@exerra.xyz>
2023-01-11 02:40:18 -0300

    :tm: • minor clean up • switched to creating RueTintCell at runtime • tint sliders & switches as well

--- RuePrefs/RueRootVC.h
+++ RuePrefs/RueRootVC.h
@@ -1,22 +1,14 @@
+@import ObjectiveC.message;
+@import ObjectiveC.runtime;
 @import Preferences.PSSpecifier;
 @import Preferences.PSTableCell;
 @import Preferences.PSListController;
 #import "Headers/Common.h"
 
-#define kRueTintColor [UIColor colorWithRed:0.74 green:0.45 blue:0.83 alpha: 1.0];
 
 @interface RueRootVC : PSListController
 @end
 
 
 @interface RueLinksVC : PSListController
-@end
-
-
-@interface PSTableCell ()
-- (void)setTitle:(NSString *)title;
-@end
-
-
-@interface RueTintCell : PSTableCell
 @end
--- RuePrefs/RueRootVC.m
+++ RuePrefs/RueRootVC.m
@@ -1,11 +1,30 @@
 #import "RueRootVC.h"
 
+
+#define kRueTintColor [UIColor colorWithRed:0.74 green:0.45 blue:0.83 alpha: 1.0];
+
 @implementation RueRootVC
 
 - (NSArray *)specifiers {
 
 	if(!_specifiers) _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
 	return _specifiers;
+
+}
+
+
+- (id)init {
+
+	self = [super init];
+	if(!self) return nil;
+
+	static dispatch_once_t token;
+	dispatch_once(&token, ^{ registerRueTintCellClass(); });
+
+	[UISlider appearanceWhenContainedInInstancesOfClasses:@[[self class]]].minimumTrackTintColor = kRueTintColor;
+	[UISwitch appearanceWhenContainedInInstancesOfClasses:@[[self class]]].onTintColor = kRueTintColor;
+
+	return self;
 
 }
 
@@ -28,10 +47,38 @@
 
 	[super setPreferenceValue:value specifier:specifier];
 
-	[NSDistributedNotificationCenter.defaultCenter postNotificationName:@"rueSetupDone" object:nil];
-	[NSDistributedNotificationCenter.defaultCenter postNotificationName:@"chooseSearchEngineDone" object:nil];
-	[NSDistributedNotificationCenter.defaultCenter postNotificationName:@"hideDockBackgroundDone" object:nil];
-	[NSDistributedNotificationCenter.defaultCenter postNotificationName:@"hideRueSearchBarBackgroundDone" object:nil];
+	[NSDistributedNotificationCenter.defaultCenter postNotificationName:RueSetupNotification object:nil];
+	[NSDistributedNotificationCenter.defaultCenter postNotificationName:RueSetupSearchEngineNotification object:nil];
+	[NSDistributedNotificationCenter.defaultCenter postNotificationName:RueHideDockBackgroundNotification object:nil];
+	[NSDistributedNotificationCenter.defaultCenter postNotificationName:RueHideSearchBarBackgroundNotification object:nil];
+
+}
+
+// ! Dark juju
+
+static void rue_setTitle(PSTableCell *self, SEL _cmd, NSString *title) {
+
+	struct objc_super superSetTitle = {
+		self,
+		[self superclass]
+	};
+
+	id (*superCall)(struct objc_super *, SEL, NSString *) = (void *)objc_msgSendSuper;
+	superCall(&superSetTitle, _cmd, title);
+
+	self.titleLabel.textColor = kRueTintColor;
+	self.titleLabel.highlightedTextColor = kRueTintColor;
+
+}
+
+static void registerRueTintCellClass() {
+
+	Class RueTintCellClass = objc_allocateClassPair([PSTableCell class], "RueTintCell", 0);
+	Method method = class_getInstanceMethod([PSTableCell class], @selector(setTitle:));
+	const char *typeEncoding = method_getTypeEncoding(method);
+	class_addMethod(RueTintCellClass, @selector(setTitle:), (IMP) rue_setTitle, typeEncoding);
+
+	objc_registerClassPair(RueTintCellClass);
 
 }
 
@@ -48,55 +95,12 @@
 }
 
 
-- (void)launchDiscord {
+- (void)launchDiscord { [self launchURL: [NSURL URLWithString: @"https://discord.gg/jbE3avwSHs"]]; }
+- (void)launchPayPal { [self launchURL: [NSURL URLWithString: @"https://paypal.me/Luki120"]]; }
+- (void)launchGitHub { [self launchURL: [NSURL URLWithString: @"https://github.com/Luki120/Rue"]]; }
+- (void)launchAria { [self launchURL: [NSURL URLWithString: @"https://luki120.github.io/depictions/web/?p=me.luki.aria"]]; }
+- (void)launchElixir { [self launchURL: [NSURL URLWithString: @"https://luki120.github.io/depictions/web/?p=me.luki.elixir"]]; }
 
-	[self launchURL: [NSURL URLWithString: @"https://discord.gg/jbE3avwSHs"]];
-
-}
-
-
-- (void)launchPayPal {
-
-	[self launchURL: [NSURL URLWithString: @"https://paypal.me/Luki120"]];
-
-}
-
-
-- (void)launchGitHub {
-
-	[self launchURL: [NSURL URLWithString: @"https://github.com/Luki120/Rue"]];
-
-}
-
-
-- (void)launchAria {
-
-	[self launchURL: [NSURL URLWithString: @"https://luki120.github.io/depictions/web/?p=me.luki.aria"]];
-
-}
-
-
-- (void)launchElixir {
-
-	[self launchURL: [NSURL URLWithString: @"https://luki120.github.io/depictions/web/?p=me.luki.elixir"]];
-
-}
-
-- (void)launchURL:(NSURL *)url {
-
-	[UIApplication.sharedApplication openURL:url options:@{} completionHandler:nil];	
-
-}
-
-@end
-
-@implementation RueTintCell
-
-- (void)setTitle:(NSString *)title {
-
-	[super setTitle: title];
-	self.titleLabel.textColor = kRueTintColor;
-
-}
+- (void)launchURL:(NSURL *)url { [UIApplication.sharedApplication openURL:url options:@{} completionHandler:nil]; }
 
 @end
